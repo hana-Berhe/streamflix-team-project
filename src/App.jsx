8f30009f3d9acde99f19166d4b3d9ad4ef7f58ce
gerdp2022 <desuy26@gmail.com>
2026-08-26 21:14:06 +0300

    Resolved merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+<<<<<<< Updated upstream
+=======
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "./assets/vite.svg";
+import Header from "./components/Header/Header";
+import "./App.css";
+>>>>>>> Stashed changes
 
 function App() {
   return (
