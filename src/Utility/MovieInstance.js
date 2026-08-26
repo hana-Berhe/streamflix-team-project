3f1420deb4c0e129664c67c457b6a63d853d266a
wondiferaw <106669830+wondiferaw@users.noreply.github.com>
2026-08-26 21:45:10 +0300

    feat: add TMDB axios instance and request endpoints

---
+++ src/Utility/MovieInstance.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const movieInstance = axios.create({
+  baseURL: "https://api.themoviedb.org/3"
+});
+
+export default movieInstance;
+//https://api.themoviedb.org/3/discover/movie?api_key=b33da15fb726a97ac2874d63f7df0e4f&with_genres=99
